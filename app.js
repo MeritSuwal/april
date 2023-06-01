14ab078cba47b07f4a861a5ecc49db246747dd1b
Merit Suwal <meritsuwal@gmail.com>
2023-06-01 21:17:44 +0545

    modal

---
+++ app.js
@@ -0,0 +1,30 @@
+// $(window).on("click", ".small", function() {
+//     me = this;
+//     setTimeout( function() { $(me).addClass("big"); }, 1 );
+// });
+
+// $(window).on("click", ".big", function() {
+//     me = this;
+//     setTimeout( function() { $(me).removeClass("big"); }, 1 );
+// });
+
+npm install react react - dom
+
+// import React from "react";
+// import "./"
+
+// const Modal = (props) => {
+//     return ( <
+//         >
+//         <
+//         div className = 'modal-backdrop' / >
+
+//         <
+//         div className = 'modal-content-wrapper' >
+//         <
+//         div className = 'modal-conten' > { props.children } <
+//         /div> < /
+//         div >
+
+//     )
+// }
--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function onTick() {
         complete();
         return;
     }
+
 }
 
 function complete() {
@@ -51,3 +52,71 @@ function comp() {
     clearInterval(tme);
     tme = null;
 }
+
+var textWrapper = document.querySelector('.intro');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.intro .letter',
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 150,
+        delay: (el, i) => 100 + 30 * i
+    });
+
+var textWrapper = document.querySelector('.design');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '.design .letter',
+        translateX: [20, 0],
+        translateZ: 0,
+        opacity: [0, 0.9],
+        easing: "easeOutExpo",
+        duration: 150,
+        delay: (el, i) => 100 + 30 * i
+    });
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var bout = document.getElementById("about");
+// bout.innerHTML = `<a href="https://www.google.com">${bout.innerHTML}</a>`;
+bout.onclick = function() {
+    modal.style.display = "block";
+}
+
+// const myDiv = document.getElementsByClassName("about");
+// const children = myDiv.children;
+// for (let i = 0; i < children.length; i++) {
+//     const child = children[i];
+//     child.innerHTML = `<a href="https://www.google.com">${child.innerHTML}</a>`;
+// }
